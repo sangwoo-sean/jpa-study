f703e23371761558b01f2b5d3fe2e32956a7340f
sangwoo <tkddn3367@gmail.com>
2022-05-05 19:47:14 +0900

    jpa initial setting

---
+++ src/main/java/com/study/repository/MemberRepository.java
@@ -0,0 +1,23 @@
+package com.study.repository;
+
+import com.study.model.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/com/study/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.study.repository;
+
+
+import com.study.model.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void testMember() {
+        //g
+        Member member = new Member();
+        member.setUsername("member");
+
+        //w
+        Long save = memberRepository.save(member);
+        Member member1 = memberRepository.find(save);
+
+        //t
+        Assertions.assertThat(member1.getId()).isEqualTo(member.getId());
+    }
+}
