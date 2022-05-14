e15391b72018a76a05e6a02dc0d846aa5ef171a6
sangwoo <tkddn3367@gmail.com>
2022-05-14 18:46:50 +0900

    회원가입시 비밀번호 암호화 with Spring Security

--- src/main/java/com/study/controller/MemberController.java
+++ src/main/java/com/study/controller/MemberController.java
@@ -6,6 +6,7 @@ import com.study.model.form.MemberForm;
 import com.study.service.MemberService;
 import com.study.validator.MemberValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,6 +22,7 @@ public class MemberController {
 
     private final MemberService memberService;
     private final MemberValidator memberValidator;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/members/new")
     public String createForm(Model model) {
@@ -36,6 +38,8 @@ public class MemberController {
         }
 
 //        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        String encoded_password = passwordEncoder.encode(memberForm.getPassword());
+        memberForm.setPassword(encoded_password);
         Member member = Member.createMember(memberForm);
 //        member.setAddress(address);
         memberService.join(member);
